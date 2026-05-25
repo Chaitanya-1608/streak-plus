ba5bbded9a883d6000d1a9c7f0941164004ad138
Chaitanya <chaitanyapachori16@gmail.com>
2026-05-25 14:54:27 +0530

    fix api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://https://streak-plus-api.onrender.com/api'
+  baseURL: 'https://streak-plus-api.onrender.com/api'
 })
 
 export default API
